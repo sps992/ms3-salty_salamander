70a285ef1c03684aa2d5a937b07a896b85aa63c6
Sam <hello@samsedgeman.co.uk>
2020-01-26 15:21:38 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env 
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipe_db'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def hello():
+    return 'HelloWorld'
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
+    debug=True)
