0853b1ef136c16593aaca7386c1753f94cf4393b
Sam <hello@samsedgeman.co.uk>
2020-02-10 21:57:34 +0000

    Added editrecipe.html and changed database names in addrecipe.html to fix bugs with new edit functionality

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 
-
 @app.route('/')
 def index():
     return render_template("index.html", recipes=mongo.db.recipes.find())
@@ -23,17 +22,14 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route('/recipe/<recipe_id>')
+def recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+    return render_template("recipe.html", recipe=recipe)
 
 @app.route('/add_recipe')
 def add_recipe():
     return render_template("addrecipe.html", categories=mongo.db.recipe_categories.find())
-
-@app.route('/edit_recipe/<recipe_id>')
-def edit_recipe(recipe_id):
-    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    all_categories = mongo.db.categories.find()
-    return render_template('editrecipe.html', recipe=the_recipe,
-                           categories=all_categories)
 
 
 @app.route('/insert_recipe', methods=['POST'])
@@ -41,6 +37,34 @@ def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('index'))
+
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.recipe_categories.find()
+    return render_template("editrecipe.html", recipe=the_recipe,
+                           categories=all_categories)
+
+
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'recipe_name': request.form.get('recipe_name'),
+        'category_name': request.form.get('category_name'),
+        'description': request.form.get('description'),
+        'serves': request.form.get('serves'),
+        'recipe_ingredients': request.form.get('recipe_ingredients'),
+        'cooking_instructions': request.form.get('cooking_instructions'),
+        'prep_time_mins': request.form.get('prep_time_mins'),
+        'cook_time_mins': request.form.get('cook_time_mins')
+    })
+    return redirect(url_for('get_tasks'))
+
+
+
 
 
 @app.route('/register')
@@ -52,15 +76,13 @@ def register():
 def login():
     return render_template("login.html")
 
+
 @app.route('/copyrights')
 def copyrights():
     return render_template("copyrights.html")
 
 
-@app.route('/recipe/<recipe_id>')
-def recipe(recipe_id):
-    recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
-    return render_template("recipe.html", recipe=recipe)
+
 
 
 if __name__ == '__main__':
